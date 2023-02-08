77d67b84f24211fe2f80e81c6b837271e9ffbd88
Nizoryu <kherzanekarim@gmail.com>
2023-02-08 19:11:56 +0100

    Parties terminéesv08-02-2023

--- Projet3/src/main/java/fr/formation/inti/controller/MainController.java
+++ Projet3/src/main/java/fr/formation/inti/controller/MainController.java
@@ -1,5 +1,28 @@
 package fr.formation.inti.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import fr.formation.inti.entity.Formulaire;
+
+@Controller
+//@RestController
 public class MainController {
 
+	
+	@GetMapping("/test")
+	public String test( Model model) {
+		return"test";
+	}
+	@GetMapping(value={"/","/index"})
+	public String index(@ModelAttribute Formulaire formulaire,  Model model) {
+		model.addAttribute("formulaire", formulaire);
+		return"index";
+	}
+	
+
+	
+	
 }
---
+++ Projet3/src/main/java/fr/formation/inti/entity/Formulaire.java
@@ -0,0 +1,81 @@
+package fr.formation.inti.entity;
+
+import java.util.Date;
+
+public class Formulaire {
+	
+	private String keywords;
+	private String city;
+	private String information;
+	private String accessibility;
+//	private Date startdate;
+//	private Date starttime;
+	
+	
+	
+	
+	public Formulaire() {
+	}
+
+
+	public String getAccessibility() {
+		return accessibility;
+	}
+
+
+	public void setAccessibility(String accessibility) {
+		this.accessibility = accessibility;
+	}
+
+
+//	public Date getStartdate() {
+//		return startdate;
+//	}
+//
+//
+//	public void setStartdate(Date startdate) {
+//		this.startdate = startdate;
+//	}
+//
+//
+//	public Date getStarttime() {
+//		return starttime;
+//	}
+//
+//
+//	public void setStarttime(Date starttime) {
+//		this.starttime = starttime;
+//	}
+
+
+	public String getInformation() {
+		return information;
+	}
+
+
+	public void setInformation(String information) {
+		this.information = information;
+	}
+
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	
+}
