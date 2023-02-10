6dde1cbce28d8c3cc50820d71e4854ff94a85a9c
somme <somme@192.168.1.25>
2023-02-10 19:33:51 +0100

    Mise à jour formulaire + edit + crud + boutons + CSS

--- Projet3/src/main/java/fr/formation/inti/controller/GestionEventController.java
+++ Projet3/src/main/java/fr/formation/inti/controller/GestionEventController.java
@@ -1,5 +1,6 @@
 package fr.formation.inti.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,8 +50,48 @@ public class GestionEventController {
 	
 	@GetMapping("/edit/{idevent}")
 	public String EditEventGET(@PathVariable Integer idevent, Model model)  {	  
+		model.addAttribute("myevent", myEventsRepository.findById(idevent).orElse(null));
+	    return "formulaireEDIT";
+	}
+	
+	
+	@PostMapping("/editEvent")
+	public String EditEventPOST(@RequestParam("idusers") Integer iduser, @ModelAttribute MyEvents event, @RequestParam("idmyevents") Integer idevent)  {	  
+		MyEvents monevent = myEventsRepository.findById(idevent).orElse(null);
 		
-	    return "formulaire";
+		System.out.println("Sysout de monevent : "+monevent);
+		monevent.setTitle(event.getTitle());
+		monevent.setPreview(event.getPreview());
+		monevent.setLongdescription(event.getLongdescription());
+		monevent.setKeywords(event.getKeywords());
+		monevent.setAdress_perso(event.getAdress_perso());
+		monevent.setAccessibility(event.getAccessibility());
+		monevent.setTimes(event.getTimes());
+		monevent.setAddress(event.getAddress());
+		monevent.setCity(event.getCity());
+		monevent.setZipcode(event.getZipcode());
+		monevent.setFirstdate_begin(event.getFirstdate_begin());
+		monevent.setFirstdate_end(event.getFirstdate_end());
+		monevent.setConditions(event.getConditions());
+		
+		
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+		String firstdate_begin = dateFormat.format(monevent.getFirstdate_begin());
+		String firstdate_end = dateFormat.format(monevent.getFirstdate_end());
+
+		String detailed_times_string = "[{\"begin\": \"" + firstdate_begin +  "\", \"end\": \"" + firstdate_end + "\"}]";
+		
+		monevent.setDetailed_times(detailed_times_string);
+		
+		
+		
+		
+		
+		
+		Integer id = monevent.getUser().getidusers();
+		myEventsRepository.save(monevent);
+	    return "redirect:/mesevenements/"+id;
 	}
 	
 	
